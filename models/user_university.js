eed30c70cd83e42cfa62c9a45b53aedaf7a2e6d2
sarahishamsaied <sarahishamsaied@gmail.com>
2023-06-28 01:07:38 +0300

    added user_university associations

--- models/user_university.js
+++ models/user_university.js
@@ -8,7 +8,14 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      user_university.belongsTo(models.university, {
+        foreignKey: "university_id",
+        as: "university",
+      });
+      user_university.belongsTo(models.user, {
+        foreignKey: "user_id",
+        as: "user",
+      });
     }
   }
   user_university.init(

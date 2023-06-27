4f379f440caff2395220db824150c0e60720b626
sarahishamsaied <sarahishamsaied@gmail.com>
2023-06-28 00:39:26 +0300

    added majors associations

--- models/major.js
+++ models/major.js
@@ -9,6 +9,10 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      major.belongsTo(models.university, {
+        foreignKey: "university_id",
+        as: "university",
+      });
     }
   }
   major.init(

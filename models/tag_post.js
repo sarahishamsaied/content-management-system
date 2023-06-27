23d3d3d688eefdaca71707e28ec2c4f31819d762
sarahishamsaied <sarahishamsaied@gmail.com>
2023-06-28 00:59:39 +0300

    added tag_post  associations

--- models/tag_post.js
+++ models/tag_post.js
@@ -8,7 +8,14 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      tag_post.belongsTo(models.tag, {
+        foreignKey: "tag_id",
+        as: "tag",
+      });
+      tag_post.belongsTo(models.post, {
+        foreignKey: "post_id",
+        as: "post",
+      });
     }
   }
   tag_post.init(

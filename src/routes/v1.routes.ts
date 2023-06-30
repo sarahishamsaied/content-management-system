b5e42572b78f3c973b748e110467206eb776121d
sarahishamsaied <sarahishamsaied@gmail.com>
2023-06-30 19:41:36 +0300

    added school routes in v1 routes

--- src/routes/v1.routes.ts
+++ src/routes/v1.routes.ts
@@ -3,11 +3,13 @@ import { Router } from "express";
 /** User Routes **/
 import userRouter from "./v1/user.routes";
 import postsRouter from "./v1/post.routes";
-import universityRouter from "./university.routes";
+import universityRouter from "./v1/university.routes";
+import schoolRouter from "./v1/school.routes";
 
 const router = Router();
 
 router.use("/users", userRouter);
 router.use("/posts", postsRouter);
 router.use("/uni", universityRouter);
+router.use("/school", schoolRouter);
 export default router;

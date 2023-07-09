0a0db84a28ccf81561b5c7abb1a8881a6e69f9e5
sarahishamsaied <sarahishamsaied@gmail.com>
2023-07-09 03:29:05 +0300

    feat: commentStore

---
+++ src/repository/comments/comment.store.ts
@@ -0,0 +1,49 @@
+import Comment from "../../../models/comment";
+
+export default class CommentStore {
+  async index(): Promise<Comment[]> {
+    try {
+      const data = await Comment.findAll({});
+      return data;
+    } catch (error) {
+      throw new Error("An Error Occurred");
+    }
+  }
+  async create(comment: Comment): Promise<Comment> {
+    try {
+      const addedComment = await Comment.create(comment);
+      return comment;
+    } catch (error) {
+      throw new Error(`an error occurred ${error}`);
+    }
+  }
+  async update(comment: Comment, id: number): Promise<Comment> {
+    try {
+      const { body } = comment;
+      const found = await Comment.findByPk(id);
+      if (!found) throw new Error("Cannot find comment");
+      found.update({ body });
+      return found;
+    } catch (error) {
+      throw new Error(`An error occurred: ${error}`);
+    }
+  }
+  async show(id: number): Promise<Comment> {
+    try {
+      const comment = await Comment.findByPk(id);
+      if (!comment) throw new Error("Comment not found");
+      return comment;
+    } catch (error) {
+      throw new Error(`${error}`);
+    }
+  }
+  async deleteComment(id: number): Promise<void> {
+    try {
+      const found = await Comment.findByPk(id);
+      if (!found) throw new Error("Comment not found");
+      await found.destroy();
+    } catch (error) {
+      throw new Error(`An error occurred: ${error}`);
+    }
+  }
+}

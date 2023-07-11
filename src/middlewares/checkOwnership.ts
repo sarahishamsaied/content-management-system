89fd052b3b27c518021472f55e444e45ab43ba01
sarahishamsaied <sarahishamsaied@gmail.com>
2023-07-12 00:22:33 +0300

    added jsdocs in ownership middleware

--- src/middlewares/checkOwnership.ts
+++ src/middlewares/checkOwnership.ts
@@ -6,28 +6,60 @@ import { UserAttributesWithId } from "../types/userTypes";
 import LikeStore from "../repository/likes/likes.store";
 import CommentStore from "../repository/comments/comment.store";
 import { ResourceOwnershipType } from "../types/ResourceOwnership";
-
-const verifyPostOwnership = async (user: UserAttributesWithId, id: number) => {
+/**
+ * Verifies ownership for a Post.
+ *
+ * @param {UserAttributesWithId} user - The user attributes with ID.
+ * @param {number} id - The ID of the post.
+ * @returns {Promise<boolean>} A promise that resolves to a boolean indicating ownership verification.
+ */
+const verifyPostOwnership = async (
+  user: UserAttributesWithId,
+  id: number
+): Promise<boolean> => {
   const postStore = new PostStore();
   const post = await postStore.show(id);
   return !!post && post.author_id === user.id;
 };
 
-const verifyLikeOwnership = async (user: UserAttributesWithId, id: number) => {
+/**
+ * Verifies ownership for a Like.
+ *
+ * @param {UserAttributesWithId} user - The user attributes with ID.
+ * @param {number} id - The ID of the like.
+ * @returns {Promise<boolean>} A promise that resolves to a boolean indicating ownership verification.
+ */
+const verifyLikeOwnership = async (
+  user: UserAttributesWithId,
+  id: number
+): Promise<boolean> => {
   const likeStore = new LikeStore();
   const like = await likeStore.show(id);
   return !!like && like.user_id === user.id;
 };
 
+/**
+ * Verifies ownership for a Comment.
+ *
+ * @param {UserAttributesWithId} user - The user attributes with ID.
+ * @param {number} id - The ID of the comment.
+ * @returns {Promise<boolean>} A promise that resolves to a boolean indicating ownership verification.
+ */
 const verifyCommentOwnership = async (
   user: UserAttributesWithId,
   id: number
-) => {
+): Promise<boolean> => {
   const commentStore = new CommentStore();
   const comment = await commentStore.show(id);
   return !!comment && comment.author_id === user.id;
 };
-
+/**
+ * Middleware to verify ownership of a resource.
+ *
+ * @param {keyof typeof ResourceOwnershipType} resource - The type of resource to verify ownership for.
+ * @returns {Function} The middleware function.
+ * @throws {Error} If an invalid resource type is provided.
+ */
 const verifyOwnership =
   (resource: keyof typeof ResourceOwnershipType) =>
   async (req: CustomRequest, res: Response, next: NextFunction) => {
